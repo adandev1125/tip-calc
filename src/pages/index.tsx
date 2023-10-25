4a3c28425b17ffeb29531803d8554dc3a4eef120
adandev1125 <adanyonai@outlook.com>
2023-10-25 11:26:11 +0300

    modified for data link page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Price from "@/components/Price";
 import RateCheck from "@/components/RateCheck";
 import { presetTipRates } from "@/constants";
 import { faDollar, faUser } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 import { useCallback, useEffect, useMemo, useState } from "react";
 
 export default function Home() {
@@ -70,78 +71,16 @@ export default function Home() {
   }, [peopleNumberText]);
 
   return (
-    <main className="flex flex-col items-center justify-center pt-[10vh] space-y-16">
+    <main className="flex flex-col items-center justify-center p-16 pt-[10vh] space-y-16 h-[100vh]">
       <div className="uppercase text-2xl font-extrabold text-[#446263] tracking-widest">
-        Spli
-        <br />
-        tter
+        Data Links
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 p-6 bg-white shadow-xl rounded-2xl w-full sm:w-[90%] md:w-[80%] lg:w-[60%]">
-        <div className="flex flex-col">
-          <span className="text-lightText">Bill</span>
-          <NumberInput
-            value={billText}
-            isError={false}
-            icon={faDollar}
-            onChange={onBillChanged}
-            placeholder="0"
-          />
-
-          <span className="text-lightText mt-10 mb-2">Select Tip %</span>
-          <div className="grid grid-cols-3 gap-2">
-            {presetTipRates.map((rate: number) => (
-              <RateCheck
-                key={rate}
-                isSelected={selectedRate === rate}
-                caption={rate * 100 + "%"}
-                onClick={onTipRateSelected.bind(null, rate)}
-              />
-            ))}
-            <NumberInput
-              value={customRateText}
-              maxValue={100}
-              isDouble={false}
-              isError={false}
-              onChange={onCustomRateChanged}
-              placeholder="Custom"
-            />
-          </div>
-
-          <div className="mt-10 flex w-full justify-between">
-            <span className="text-lightText">Number of People</span>
-            <span
-              className={
-                "text-red-600 transition-all" +
-                (peopleInputError.length > 0 ? "opacity-100" : "opacity-0")
-              }
-            >
-              {peopleInputError}
-            </span>
-          </div>
-          <NumberInput
-            value={peopleNumberText}
-            isDouble={false}
-            isError={peopleInputError.length > 0}
-            onChange={onNumberOfPeopleChanged}
-            icon={faUser}
-            placeholder="0"
-          />
-        </div>
-
-        <div className="flex flex-col w-full h-full bg-[#1b474b] rounded-xl px-8 py-12 justify-between">
-          <div>
-            <Price caption="Tip Amount" price={prices.tipAmount} />
-            <Price caption="Total" price={prices.total} />
-          </div>
-          <button
-            className="w-full bg-buttonActive text-buttonNormal uppercase disabled:opacity-50"
-            disabled={prices.tipAmount <= 0 && prices.total <= 0}
-            onClick={onReset}
-          >
-            Reset
-          </button>
-        </div>
+      <div className="container">
+        <span className="text-lightText">2023-10-17 Data</span>
+        <Link href={"https://drive.google.com/file/d/1NXYYYC0cjvpCoQou7D3bLpVOl-qzm10t/view?usp=drive_link"} target="blank">
+          https://drive.google.com/file/d/1NXYYYC0cjvpCoQou7D3bLpVOl-qzm10t/view?usp=drive_link
+        </Link>
       </div>
     </main>
   );
